b33b433dc5241dd4d46994e03205684eab3a7a04
Emotional-Sxpport <coleseley@gmail.com>
2025-11-06 09:18:31 -0800

    adding action events, not working yet

--- src/ChaseTheAce.java
+++ src/ChaseTheAce.java
@@ -1,9 +1,14 @@
 import javax.swing.*;
 import java.awt.*;
 import java.util.Random;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class ChaseTheAce extends JFrame {
 
+    private int test = 0;
     public ChaseTheAce() {
         setTitle("Chase the Ace");
         setSize(2100, 1400);
@@ -22,14 +27,29 @@ public class ChaseTheAce extends JFrame {
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
 
-            //PlayingCard card = new PlayingCard();
+            PlayingCard card = new PlayingCard();
             //Player player = new Player();
             System system = new System();
             for (int i = 0; i < 4; i++) {
                 system.getPlayer(i).draw(100 + 150 * i, 100, g);
             }
             //player.draw(200, 200, g);
+            if (test == 1)
+                card.draw(300, 300, g);
 
+        }
+    }
+
+    //@Override
+    public void actionPerformed(KeyEvent e) {
+        // This method is called when an action occurs (e.g., button click)
+        int keyCode = e.getKeyCode();
+        if (keyCode == KeyEvent.VK_ENTER) {
+            //System.out.println("Enter key pressed!");
+            test = 1;
+
+        } else if (keyCode == KeyEvent.VK_SPACE) {
+            //System.out.println("Space bar pressed!");
         }
     }
 
