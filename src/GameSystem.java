111710a3c51b41078799bbb598fa469a55b668e3
msheafor <mirakestrel12@gmail.com>
2025-11-09 14:19:05 -0800

    Testing

--- src/GameSystem.java
+++ src/GameSystem.java
@@ -54,7 +54,7 @@ public class GameSystem {
 
         for (int i = 0; i < playerCount; i++) {
             if(turn % playerCount == 0){
-                //Hi Cole again
+                //Hi Cole again :)
                 //Generate the buttons
                 //Wait for the player to click
                 //if trade, call trade
